5602875a7ce85e27c45f00fb0a7ae9d4783bb90d
captainwycliffe <captainwycliffe@gmail.com>
2025-05-21 20:25:08 +0300

    switched from Sqlite3 to PostgreSQL for hosting to render

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_05_01_084407) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tweeets", force: :cascade do |t|
     t.text "tweeet"
     t.datetime "created_at", null: false
